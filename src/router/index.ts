85b5fc917be29203cc8e44ab7cdec100a5125384
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-05 00:43:21 -0300

    refatora rotas do roteador: substitui dashboardRoute por mainRoute

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { Router } from "vue-router";
-import { dashboardRoute } from "../modules/dashboard/route";
+import { mainRoute } from "../modules/main/route";
 import { authRoute } from "../modules/auth/route";
 
 const router: Router = createRouter({
   history: createWebHistory(),
-  routes: [...dashboardRoute, authRoute],
+  routes: [mainRoute, authRoute],
 });
 
 export default router;
