843c45d94a0991a4f88cf911afb08704ab7e1344
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-05 19:39:07 -0300

    adiciona componente SideMenu: implementa o menu lateral com opções de navegação e informações do usuário

--- src/modules/main/Main.page.vue
+++ src/modules/main/Main.page.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useUser } from "@/modules/main/hooks/user.hook";
 import { onMounted } from "vue";
+import SideMenu from "./components/SideMenu.vue";
 
 const { getUser } = useUser();
 
@@ -9,5 +10,8 @@ onMounted(async () => {
 });
 </script>
 <template>
-  <RouterView />
+  <section class="flex w-full h-screen">
+    <SideMenu />
+    <RouterView />
+  </section>
 </template>
---
+++ src/modules/main/components/SideMenu.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Menu from "primevue/menu";
+import Avatar from "primevue/avatar";
+import Button from "primevue/button";
+import { useUser } from "../hooks/user.hook";
+import { useMain } from "../hooks/main.hook";
+
+const { user } = useUser();
+const { logout } = useMain();
+
+const items = ref([
+  {
+    items: [
+      {
+        label: "Dashboard",
+        icon: "dashboard",
+      },
+      {
+        label: "Clientes",
+        icon: "groups",
+      },
+    ],
+  },
+]);
+</script>
+<template>
+  <div class="h-full p-4">
+    <Menu
+      :model="items"
+      class="!h-full !flex !flex-col !bg-surface-50"
+      pt:list="flex-1"
+    >
+      <template #start>
+        <div class="flex items-center justify-center gap-2 py-4">
+          <span class="material-symbols-outlined !text-5xl text-primary-900">
+            network_intel_node
+          </span>
+          <span class="text-xl font-bold text-primary-900"> MindFlux </span>
+        </div>
+      </template>
+      <template #item="{ item, props }">
+        <a class="flex items-center font-semibold" v-bind="props.action">
+          <i class="material-icons">{{ item.icon }}</i>
+          <span>{{ item.label }}</span>
+        </a>
+      </template>
+      <template #end>
+        <div class="flex flex-col p-2 gap-2">
+          <div
+            class="w-full flex items-center gap-2 p-2 hover:bg-primary-100 rounded-lg cursor-pointer transition-all duration-100"
+          >
+            <Avatar icon="pi pi-user" size="large" shape="circle" />
+            <div>
+              <p class="font-semibold">{{ user?.name || "Meu Perfil" }}</p>
+              <p>{{ user?.email }}</p>
+            </div>
+          </div>
+          <Button label="Sair" icon="pi pi-sign-out" text @click="logout" />
+        </div>
+      </template>
+    </Menu>
+  </div>
+</template>
