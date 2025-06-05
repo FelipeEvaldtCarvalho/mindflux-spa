650cebfefb6eff02efc7d5ff789b71bb4d1ffe79
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-05 17:37:53 -0300

    adiciona guarda de autenticação: implementa a função setupAuthGuard para verificar o token de usuário antes de acessar rotas de autenticação

---
+++ src/router/auth.guard.ts
@@ -0,0 +1,20 @@
+import type { Router } from "vue-router";
+import apiService from "@/services/ApiService";
+
+export function setupAuthGuard(router: Router) {
+  router.beforeEach(async (to, _, next) => {
+    if (to.path.startsWith("/auth")) {
+      const token = localStorage.getItem("user_token");
+      if (!token) next();
+      try {
+        await apiService.axios.get("/auth/verify-token");
+        return next("/");
+      } catch {
+        localStorage.removeItem("user_token");
+        return next();
+      }
+    }
+
+    next();
+  });
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import type { Router } from "vue-router";
 import { mainRoute } from "../modules/main/route";
 import { authRoute } from "../modules/auth/route";
+import { setupAuthGuard } from "./auth.guard";
 
 const router: Router = createRouter({
   history: createWebHistory(),
   routes: [mainRoute, authRoute],
 });
+
+setupAuthGuard(router);
 
 export default router;
