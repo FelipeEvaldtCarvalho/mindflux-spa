0e293caff776b4bf8fd76ec2cc3978bcf63e64d5
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-05 00:42:59 -0300

    adiciona suporte a tipos para Node e configura alias no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const getUrl = () => new URL("./src", import.meta.url).pathname;
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": getUrl(),
+    },
+  },
 });
