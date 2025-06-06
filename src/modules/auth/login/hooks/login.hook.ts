59927c8d2896580fd3b1bc908e6a957c70aef3cc
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-06 18:29:53 -0300

    adiciona tratamento de erro no login: implementa exibição de mensagem de erro ao falhar na autenticação

--- src/modules/auth/login/hooks/login.hook.ts
+++ src/modules/auth/login/hooks/login.hook.ts
@@ -1,6 +1,7 @@
 import { reactive, toRefs, computed } from "vue";
 import service from "../services";
-import router from "../../../../router";
+import { useToast } from "primevue/usetoast";
+import { useRouter } from "vue-router";
 
 const state = reactive({
   isSubmitting: false,
@@ -8,31 +9,39 @@ const state = reactive({
   password: "",
 });
 
-const login = async () => {
-  try {
-    if (isDisabledButton.value) return;
-    const payload = {
-      email: state.email,
-      password: state.password,
-    };
-    state.isSubmitting = true;
-    const { token } = await service.login(payload);
-    localStorage.setItem("user_token", token);
-    router.push({ name: "Home" });
-  } catch (error) {
-    console.error("Login failed:", error);
-  } finally {
-    state.isSubmitting = false;
-    state.email = "";
-    state.password = "";
-  }
-};
-
 const isDisabledButton = computed(() => {
   return !state.email || !state.password || state.isSubmitting;
 });
 
 export const useLogin = () => {
+  const toast = useToast();
+  const router = useRouter();
+
+  const login = async () => {
+    try {
+      if (isDisabledButton.value) return;
+      const payload = {
+        email: state.email,
+        password: state.password,
+      };
+      state.isSubmitting = true;
+      const { token } = await service.login(payload);
+      localStorage.setItem("user_token", token);
+      router.push({ name: "Home" });
+    } catch (error) {
+      toast.add({
+        severity: "error",
+        summary: "Erro!",
+        detail: "Credenciais inválidas.",
+        life: 4000,
+      });
+    } finally {
+      state.isSubmitting = false;
+      state.email = "";
+      state.password = "";
+    }
+  };
+
   return {
     ...toRefs(state),
     login,
