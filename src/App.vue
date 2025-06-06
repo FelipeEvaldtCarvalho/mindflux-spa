f96482e1e3fdfb7766a4dba70d2580afdd0dda88
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-06 18:29:35 -0300

    adiciona Toast e ToastService: integra o componente Toast e o serviço de Toast do PrimeVue no aplicativo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Toast from "primevue/toast";
 </script>
 <template>
+  <Toast />
   <RouterView />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import PrimeVue, { primeVueOptions } from "./plugins/primevue";
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
+import ToastService from "primevue/toastservice";
 
 import "primeicons/primeicons.css";
 import "./style.css";
@@ -9,7 +10,6 @@ import "./style.css";
 const app = createApp(App);
 
 app.use(PrimeVue, primeVueOptions);
-
 app.use(router);
-
+app.use(ToastService);
 app.mount("#app");
