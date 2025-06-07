3211a70119cbafeb30b63022396b8e88a0869ff2
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-07 02:38:29 -0300

    adiciona componente FeedBack: cria o componente FeedBack com propriedades para título, ícone e descrição

---
+++ src/components/FeedBack.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  icon?: string;
+  description?: string;
+}>();
+</script>
+
+<template>
+  <div class="p-6 flex flex-col items-center gap-2">
+    <div
+      v-if="icon"
+      class="bg-primary-200 rounded-full p-4 aspect-square flex items-center justify-center"
+    >
+      <span class="material-symbols-outlined !text-5xl text-white">
+        {{ icon }}
+      </span>
+    </div>
+    <p v-if="title" class="text-xlg font-semibold">
+      {{ title }}
+    </p>
+    <p v-if="description" class="text-sm text-gray-600">
+      {{ description }}
+    </p>
+    <slot />
+  </div>
+</template>
