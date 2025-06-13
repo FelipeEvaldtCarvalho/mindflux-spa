2b18fd85cb6f438cd008c7d5a30f24564483a93b
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-13 19:01:08 -0300

    adiciona funcionalidade de expansão e minimização da barra lateral: implementa lógica para alternar a exibição da barra lateral e ajusta o layout do menu com base no tamanho da tela

--- src/modules/main/components/SideMenu.vue
+++ src/modules/main/components/SideMenu.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import Menu from "primevue/menu";
 import Avatar from "primevue/avatar";
 import Button from "primevue/button";
 import { useUser } from "../hooks/user.hook";
 import { useMain } from "../hooks/main.hook";
 import { useRouter } from "vue-router";
+import { useBreakpoint } from "@/hooks/mq.hook";
+
 const router = useRouter();
+const { breakpoint, isLgAndUp } = useBreakpoint();
 
 const { user } = useUser();
 const { logout } = useMain();
+
+const isSidebarExpanded = ref(true);
+
+const toggleSidebar = () => {
+  isSidebarExpanded.value = !isSidebarExpanded.value;
+};
+
+const isMenuOpen = ref(false);
+
+const showMenu = computed(() => {
+  return isLgAndUp() || isMenuOpen.value;
+});
+
+const isMinimizedSidebar = computed(() => {
+  return !isSidebarExpanded.value && isLgAndUp();
+});
 
 const items = ref([
   {
@@ -29,43 +48,114 @@ const items = ref([
 ]);
 </script>
 <template>
-  <div class="h-full p-4">
+  <div
+    id="side-menu"
+    class="sticky top-0 left-0 z-10 lg:h-screen lg:p-2"
+    :class="{
+      'lg:w-fit lg:min-w-fit': isMinimizedSidebar,
+      'lg:w-[260px] lg:min-w-[260px]': !isMinimizedSidebar,
+    }"
+  >
+    <div
+      class="bg-primary-100 flex p-6 w-full justify-between items-center text-primary-800 text-xl font-semibold lg:hidden !overflow-hidden"
+      v-if="!isLgAndUp()"
+    >
+      <div class="flex items-center gap-2 cursor-pointer">
+        <span class="material-symbols-outlined !text-4xl">
+          network_intel_node
+        </span>
+        MindFlux
+        {{ breakpoint }}
+      </div>
+      <i class="material-icons cursor-pointer" @click="isMenuOpen = !isMenuOpen"
+        >grid_view</i
+      >
+    </div>
     <Menu
+      appendTo="#side-menu"
+      v-if="showMenu"
       :model="items"
-      class="!h-full !flex !flex-col !bg-surface-50"
-      pt:list="flex-1"
+      class="absolute w-full h-screen top-0 left-0 z-20 flex flex-col lg:static lg:!h-[-webkit-fill-available]"
+      :class="{
+        '!min-w-fit': !isSidebarExpanded,
+        '': isSidebarExpanded,
+      }"
+      pt:list="flex-1 !p-4"
+      pt:submenulabel="hidden"
+      pt:start="flex p-6 w-full justify-between items-center text-primary-800 text-xl font-semibold relative"
+      pt:end="grid p-6 w-full !p-4 gap-2"
     >
       <template #start>
-        <div class="flex items-center justify-center gap-2 py-4">
-          <span class="material-symbols-outlined !text-5xl text-primary-900">
+        <div class="flex items-center gap-2 cursor-pointer">
+          <span class="material-symbols-outlined !text-4xl">
             network_intel_node
           </span>
-          <span class="text-xl font-bold text-primary-900"> MindFlux </span>
+          <span v-if="!isMinimizedSidebar" class="">
+            MindFlux
+            {{ breakpoint }}
+          </span>
+        </div>
+        <i
+          class="material-icons cursor-pointer lg:!hidden"
+          @click="isMenuOpen = false"
+          >close</i
+        >
+        <div
+          class="hidden lg:!flex items-center justify-center absolute bg-surface-200 hover:!bg-primary-300 p-px rounded-full aspect-square -right-3 -bottom-3 cursor-pointer"
+          @click="toggleSidebar"
+        >
+          <i
+            class="material-icons"
+            :class="{
+              'rotate-180': isSidebarExpanded,
+            }"
+            >chevron_right</i
+          >
         </div>
       </template>
       <template #item="{ item, props }">
         <a
-          class="flex items-center font-semibold"
+          class="flex items-center gap-2"
+          :class="{
+            'justify-center': !isSidebarExpanded,
+            '': isSidebarExpanded,
+          }"
           v-bind="props.action"
           @click="router.push(item.to)"
         >
           <i class="material-icons">{{ item.icon }}</i>
-          <span>{{ item.label }}</span>
+          <span v-if="!isMinimizedSidebar">{{ item.label }}</span>
         </a>
       </template>
       <template #end>
-        <div class="flex flex-col p-2 gap-2">
-          <div
-            class="w-full flex items-center gap-2 p-2 hover:bg-primary-100 rounded-lg cursor-pointer transition-all duration-100"
-          >
-            <Avatar icon="pi pi-user" size="large" shape="circle" />
-            <div>
-              <p class="font-semibold">{{ user?.name || "Meu Perfil" }}</p>
-              <p>{{ user?.email }}</p>
+        <div
+          class="hover:bg-primary-50 rounded-lg cursor-pointer overflow-hidden p-3"
+        >
+          <div class="flex lg:flex-col w-full gap-4 lg:gap-2">
+            <Avatar icon="pi pi-user" shape="circle" class="m-auto" />
+            <div class="flex flex-col w-full gap-1">
+              <p
+                class="overflow-hidden text-lg font-semibold text text-nowrap !text-ellipsis"
+                v-if="!isMinimizedSidebar"
+              >
+                {{ user?.name || "Meu Perfil" }}
+              </p>
+              <p
+                class="overflow-hidden text-nowrap text-ellipsis"
+                v-if="!isMinimizedSidebar"
+              >
+                {{ user?.email }}
+              </p>
             </div>
           </div>
-          <Button label="Sair" icon="pi pi-sign-out" text @click="logout" />
         </div>
+        <Button
+          :label="isSidebarExpanded ? 'Sair' : ''"
+          icon="pi pi-sign-out"
+          text
+          @click="logout"
+          class="!w-full"
+        />
       </template>
     </Menu>
   </div>
