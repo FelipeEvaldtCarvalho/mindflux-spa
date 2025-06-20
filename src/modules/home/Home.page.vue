a1cc4bc84a5a3f791e3f97bb466a1d3524efd59b
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-19 23:31:10 -0300

    feat: integra tratamento de carregamento e erros na página inicial com componentes visuais de feedback

--- src/modules/home/Home.page.vue
+++ src/modules/home/Home.page.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Card from "primevue/card";
+import Skeleton from "primevue/skeleton";
+import Button from "primevue/button";
 import { useDashboard } from "./hooks/dashboard.hook";
 
-const { dashboardData } = useDashboard();
+const { dashboardData, isLoading, hasError, getData } = useDashboard();
 </script>
 <template>
   <section class="grid md:grid-cols-2 lg:grid-cols-3 m-6 gap-4 h-fit">
@@ -11,7 +13,16 @@ const { dashboardData } = useDashboard();
         <h2 class="text-lg font-semibold">Clientes cadastrados:</h2>
       </template>
       <template #content>
-        <p class="text-4xl font-bold">{{ dashboardData?.customers }}</p>
+        <Button
+          v-if="hasError"
+          label="Tentar novamente"
+          @click="getData"
+          icon="pi pi-refresh"
+          size="small"
+          class="h-[40px]"
+        />
+        <Skeleton v-else-if="isLoading" width="100px" height="40px" />
+        <p v-else class="text-4xl font-bold">{{ dashboardData?.customers }}</p>
       </template>
     </Card>
   </section>
